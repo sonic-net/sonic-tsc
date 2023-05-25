d7de7feef83b88d550db08bf55b3b375030e4e64
Shilong Liu <shilongliu@microsoft.com>
2023-05-25 05:30:54 +0000

    Update author.csv

---
+++ sii_author_map/merge_contributors.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+csv=author.csv
+
+./json2csv.sh contributors.json > tmp.csv
+while IFS= read -r line;do
+    id=$(echo $line | awk -F, '{print$1}')
+    if grep "^$id," tmp.csv 2>/dev/null; then
+        continue
+    fi
+
+    echo $line >> tmp.csv
+done < $csv
+
+sort tmp.csv > tmp
+mv tmp $csv
