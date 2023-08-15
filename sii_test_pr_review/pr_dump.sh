9bc6277637bafd35d770f7570979b3860df6a075
Shilong Liu <shilongliu@microsoft.com>
2023-08-15 08:17:31 +0000

    Dump test pr/review data for 2023 to predict next years' score

--- sii_test_pr_review/pr_dump.sh
+++ sii_test_pr_review/pr_dump.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-../sii_pr_review/pr_dump.sh -r sonic-net/sonic-mgmt -k ,files $@
+../sii_pr_review/pr_dump.sh -r sonic-net/sonic-mgmt -k ,files -y 2023 -m -t prs
+
+../sii_pr_review/pr_dump.sh -r sonic-net/sonic-mgmt -k ,files -y 2023 -m -t reviews
