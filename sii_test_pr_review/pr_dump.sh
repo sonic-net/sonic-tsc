209c6a97c3ddac1718bf03b21f8d0fa72e929f45
Shilong Liu <shilongliu@microsoft.com>
2023-12-21 05:58:24 +0000

    Add dump data pipeline and update script

--- sii_test_pr_review/pr_dump.sh
+++ sii_test_pr_review/pr_dump.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-../sii_pr_review/pr_dump.sh -r sonic-net/sonic-mgmt -k ,files -y 2023 -m -t prs
 
-../sii_pr_review/pr_dump.sh -r sonic-net/sonic-mgmt -k ,files -y 2023 -m -t reviews
+../sii_pr_review/pr_dump.sh -r sonic-net/sonic-mgmt -k ,files -m -t prs "$@"
+
+../sii_pr_review/pr_dump.sh -r sonic-net/sonic-mgmt -k ,files -m -t reviews "$@"
